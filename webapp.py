43e3a8d7a847bb0abf96da5bf8ef0a0ffeab5711
Monika <lumimonika1@gmail.com>
2024-04-09 18:47:54 -1200

    Scrape temperature data from the link webpage create webbapp to show date and temperature with graphic with each values date/time and temperature

--- webapp.py
+++ webapp.py
@@ -5,6 +5,7 @@ import pandas
 
 df = pandas.read_csv("data.txt")
 
-figure = px.line(x=df["date"], y=df["temperature"], labels={"x":"Date", "y":"Temperature (C)"})
+figure = px.line(x=df["date"], y=df["temperature"],
+                             labels={"x": "Date", "y": "Temperature (C)"})
 
 st.plotly_chart(figure)
